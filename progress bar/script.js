e2cc10ac6250a164ec7c61fad4f276f678dbf389
Zixianlow <113102979+Zixianlow@users.noreply.github.com>
2023-10-14 14:04:30 +0800

    Added scroll top, progress bar, fix custom cursor 1. Scroll top makes page automatically scroll to top when you hit bottom 2. Progress bar shows percentage of life and current status 3. Custom cursor was only avaiable for non scrolled part

---
+++ progress bar/script.js
@@ -0,0 +1,26 @@
+function updateProgressBar(percentage) {
+    const progressBar = document.getElementById("myProgressBar");
+    progressBar.style.width = percentage + "%";
+    if (percentage <= 30)
+        progressBar.innerHTML = percentage + "%<br>(Childhood)";
+    else if (percentage <= 70)
+        progressBar.innerHTML = percentage + "%<br>(Adult)";
+    else
+        progressBar.innerHTML = percentage + "%<br>(Old)";
+}
+
+let currentProgress = 0;
+const targetProgress = 100;
+const duration = 120000; // 12 seconds in milliseconds
+const increment = 1; // Increase by 1% on each interval
+const intervalDuration = (duration / targetProgress) * increment;
+
+// Update the progress bar at regular intervals
+const progressBarInterval = setInterval(function() {
+    currentProgress += increment;
+    if (currentProgress <= targetProgress) {
+        updateProgressBar(currentProgress);
+    } else {
+        clearInterval(progressBarInterval);
+    }
+}, intervalDuration);
---
+++ top scroll/script.js
@@ -0,0 +1,14 @@
+window.addEventListener("scroll", () => {
+    // Calculate how far down the page is scrolled
+    const scrollHeight = document.documentElement.scrollHeight;
+    const clientHeight = window.innerHeight;
+    const scrollPosition = window.scrollY;
+
+    // Set a threshold for scrolling (e.g., 90% from the top)
+    const scrollThreshold = scrollHeight - clientHeight;
+
+    // If the scroll position is near the bottom, scroll to the top
+    if (scrollPosition >= scrollThreshold * 0.99) {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+});
